92db32037e7555e2587cda4cc2ca5ee92d8b0650
Matt Press <matt.j.press@gmail.com>
2018-11-06 23:56:07 +0000

    Fix chart tests

--- src/AppleMusicAPI.NET.Tests/IntegrationTests/DeserializationTests/DeserializationTestBase.cs
+++ src/AppleMusicAPI.NET.Tests/IntegrationTests/DeserializationTests/DeserializationTestBase.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AppleMusicAPI.NET.Utilities;
+﻿using AppleMusicAPI.NET.Utilities;
 using Xunit;
 
 namespace AppleMusicAPI.NET.Tests.IntegrationTests.DeserializationTests
